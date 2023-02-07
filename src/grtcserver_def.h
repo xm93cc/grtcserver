1c6b77b9e43e1ec33cd6fb2889f795bc8b2299d8
xm93cc <xm93cc@foxmail.com>
2023-02-07 18:38:53 +0800

    补充常量定义

---
+++ src/grtcserver_def.h
@@ -0,0 +1,22 @@
+#ifndef __GRTCSERVER_DEF_H_
+#define __GRTCSERVER_DEF_H_
+
+#define CMDNO_PUSH     1
+#define CMDNO_PULL     2
+#define CMDNO_ANSWER   3
+#define CMDNO_STOPPUSH 4
+#define CMDNO_STOPPULL 5
+
+namespace grtc
+{
+    struct RtcMsg{
+        int cmdno = -1;
+        uint64_t uid = 0;
+        std::string stream_name;
+        int audio = 0;
+        int video = 0;
+    };
+} // namespace grtc
+
+
+#endif //__GRTCSERVER_DEF_H_
