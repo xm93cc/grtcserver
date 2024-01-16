7a4e56d9199732b0d19c29509ee8fc5020ea5921
xm93cc <xm93cc@foxmail.com>
2024-01-16 20:34:57 +0800

    缓存ClientHello packet

--- src/pc/dtls_transport.cpp
+++ src/pc/dtls_transport.cpp
@@ -3,6 +3,9 @@
 #include "pc/dtls_transport.h"
 
 namespace grtc {
+
+const size_t k_dtls_record_header_len = 13;
+
 DtlsTransport::DtlsTransport(IceTransportChannel* ice_channel)
     : _ice_channel(ice_channel) {
         _ice_channel->signal_read_packet.connect(this, &DtlsTransport::_on_read_packet);
@@ -11,10 +14,64 @@ DtlsTransport::~DtlsTransport() {
     //temp none
 }
 
-void DtlsTransport::_on_read_packet(IceTransportChannel* /*channel*/, const char* /*buf*/, size_t len, int64_t /*ts*/){
-    RTC_LOG(LS_INFO) << "============DTLS: packet: " << len;
+bool DtlsTransport::is_dtls_packet(const char* buf, size_t len) {
+  const uint8_t* u = reinterpret_cast<const uint8_t*>(buf);
+  return len >= k_dtls_record_header_len && (u[0] > 19 && u[0] < 64);
 }
 
+bool DtlsTransport::is_dtls_client_hello_packet(const char* buf, size_t len) {
+  if (!is_dtls_packet(buf, len)) {
+    return false;
+  }
+
+  const uint8_t* u = reinterpret_cast<const uint8_t*>(buf);
+  return len > 17 && (u[0] == 22 && u[13] < 1);
+}
+
+void DtlsTransport::_on_read_packet(IceTransportChannel* /*channel*/,
+                                    const char* buf, size_t len,
+                                    int64_t /*ts*/) {
+  switch (_dtls_state) {
+    case DtlsTransportState::k_new:
+      if (_dtls) {
+        RTC_LOG(LS_INFO) << to_string()
+                         << ": Received packet before DTLS started. ";
+      } else {
+        RTC_LOG(LS_INFO) << to_string()
+                         << ": Received packet before we know if "
+                         << "we are doing DTLS or not ";
+      }
+      if (is_dtls_client_hello_packet(buf, len)) {
+        RTC_LOG(LS_INFO) << to_string()
+                         << ": Caching DTLS ClientHello packet until "
+                         << "DTLS started";
+        _cached_client_hello.SetData(buf, len);
+        if(!_dtls && _local_certificate){
+            _setup_dtls();
+        }
+      } else {
+        RTC_LOG(LS_INFO) << to_string() << ": Not a DTLS ClientHello packet, "
+                         << "dropping";
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+std::string DtlsTransport::to_string() {
+  std::stringstream ss;
+  absl::string_view RECEIVING[2] = {"-", "R"};
+  absl::string_view WRITABLE[2] = {"-", "R"};
+  ss << "DtlsTransport[" << transport_name() << "|" << (int)component() << "|"
+     << RECEIVING[_receiving] << "|" << WRITABLE[_writable] << "]";
+  return ss.str();
+}
+
+bool DtlsTransport::_setup_dtls() {
+    return false;
+}
 
 const std::string& DtlsTransport::transport_name(){
   return _ice_channel->transport_name();
--- src/pc/dtls_transport.h
+++ src/pc/dtls_transport.h
@@ -1,9 +1,24 @@
 #ifndef __DTLS__TRANSPORT_H_
 #define __DTLS__TRANSPORT_H_
+
+#include <memory>
+#include <rtc_base/ssl_stream_adapter.h>
+#include <rtc_base/buffer_queue.h>
+#include <rtc_base/rtc_certificate.h>
 #include "ice/ice_transport_channel.h"
 
 namespace grtc
 {
+
+enum class DtlsTransportState{
+    k_new,
+    k_connecting,
+    k_connected,
+    k_closed,
+    k_failed,
+    k_num_values
+};
+
 class DtlsTransport : public sigslot::has_slots<>{
 public:
     DtlsTransport(IceTransportChannel* ice_channel);
@@ -13,11 +28,26 @@ public:
     const std::string& transport_name();
 
     IceCandidateComponent component() {return _ice_channel->component();}
+    
+    std::string to_string();
+
+    bool is_dtls_client_hello_packet(const char* buf, size_t len);
+
+    bool is_dtls_packet(const char* buf, size_t len);
 
 private:
     void _on_read_packet(IceTransportChannel* channel, const char* buf, size_t len, int64_t ts);
+
+    bool _setup_dtls();
+
 private:
     IceTransportChannel* _ice_channel;   
+    DtlsTransportState _dtls_state = DtlsTransportState::k_new;
+    bool _receiving = false;
+    bool _writable = false;
+    std::unique_ptr<rtc::SSLStreamAdapter> _dtls;
+    rtc::Buffer _cached_client_hello;
+    rtc::RTCCertificate* _local_certificate = nullptr;
 };
 } // namespace grtc
 
