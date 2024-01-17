09b65cdf60300080b17103c2aa18e7269f5a933d
xm93cc <xm93cc@foxmail.com>
2024-01-17 21:47:26 +0800

    安装DTLS

--- src/pc/dtls_transport.cpp
+++ src/pc/dtls_transport.cpp
@@ -70,11 +70,61 @@ std::string DtlsTransport::to_string() {
 }
 
 bool DtlsTransport::_setup_dtls() {
-    return false;
+    auto downward = std::make_unique<StreamInterfaceChannel>(_ice_channel);
+    StreamInterfaceChannel* downward_ptr = downward.get();
+    _dtls = rtc::SSLStreamAdapter::Create(std::move(downward));
+    if(!_dtls){
+        RTC_LOG(LS_WARNING) << to_string() << ": Failed to create SSLStreamAdapter";
+        return false;
+    }
+    _downward = downward_ptr;
+    _dtls->SetIdentity(_local_certificate->identity()->Clone());
+    _dtls->SetMode(rtc::SSL_MODE_DTLS);
+    _dtls->SetMaxProtocolVersion(rtc::SSL_PROTOCOL_DTLS_12);
+    _dtls->SetServerRole(rtc::SSL_SERVER);
+    if(_remote_fingerprint_value.size() && !_dtls->SetPeerCertificateDigest(_remote_fingerprint_alg, _remote_fingerprint_value.data(),_remote_fingerprint_value.size())){
+        RTC_LOG(LS_WARNING) << to_string() << ": Failed to set remote fingerprint";
+        return false;
+    }
+    RTC_LOG(LS_INFO) << to_string() << ": Setup DTLS complete";
+    _maybe_start_dtls(); 
+    return true;
+}
+
+bool DtlsTransport::_maybe_start_dtls(){
+    return true;
 }
 
 const std::string& DtlsTransport::transport_name(){
   return _ice_channel->transport_name();
 }
+
+StreamInterfaceChannel::StreamInterfaceChannel(IceTransportChannel* ice_channel)
+    : _ice_channel(ice_channel) {}
+
+rtc::StreamState StreamInterfaceChannel::GetState() const{}
+
+// Read attempts to fill buffer of size buffer_len.  Write attempts to send
+// data_len bytes stored in data.  The variables read and write are set only
+// on SR_SUCCESS (see below).  Likewise, error is only set on SR_ERROR.
+// Read and Write return a value indicating:
+//  SR_ERROR: an error occurred, which is returned in a non-null error
+//    argument.  Interpretation of the error requires knowledge of the
+//    stream's concrete type, which limits its usefulness.
+//  SR_SUCCESS: some number of bytes were successfully written, which is
+//    returned in a non-null read/write argument.
+//  SR_BLOCK: the stream is in non-blocking mode, and the operation would
+//    block, or the stream is in SS_OPENING state.
+//  SR_EOS: the end-of-stream has been reached, or the stream is in the
+//    SS_CLOSED state.
+rtc::StreamResult StreamInterfaceChannel::Read(void* buffer, size_t buffer_len,
+                                               size_t* read, int* error) {}
+
+rtc::StreamResult StreamInterfaceChannel::Write(const void* data,
+                                                size_t data_len,
+                                                size_t* written, int* error) {}
+// Attempt to transition to the SS_CLOSED state.  SE_CLOSE will not be
+// signalled as a result of this call.
+void StreamInterfaceChannel::Close() {}
 
 }  // namespace grtc
--- src/pc/dtls_transport.h
+++ src/pc/dtls_transport.h
@@ -19,6 +19,38 @@ enum class DtlsTransportState{
     k_num_values
 };
 
+class StreamInterfaceChannel : public rtc::StreamInterface {
+ public:
+  StreamInterfaceChannel(IceTransportChannel* ice_channel);
+
+  rtc::StreamState GetState() const override;
+
+  // Read attempts to fill buffer of size buffer_len.  Write attempts to send
+  // data_len bytes stored in data.  The variables read and write are set only
+  // on SR_SUCCESS (see below).  Likewise, error is only set on SR_ERROR.
+  // Read and Write return a value indicating:
+  //  SR_ERROR: an error occurred, which is returned in a non-null error
+  //    argument.  Interpretation of the error requires knowledge of the
+  //    stream's concrete type, which limits its usefulness.
+  //  SR_SUCCESS: some number of bytes were successfully written, which is
+  //    returned in a non-null read/write argument.
+  //  SR_BLOCK: the stream is in non-blocking mode, and the operation would
+  //    block, or the stream is in SS_OPENING state.
+  //  SR_EOS: the end-of-stream has been reached, or the stream is in the
+  //    SS_CLOSED state.
+  rtc::StreamResult Read(void* buffer, size_t buffer_len, size_t* read,
+                         int* error) override;
+
+  rtc::StreamResult Write(const void* data, size_t data_len, size_t* written,
+                          int* error) override;
+  // Attempt to transition to the SS_CLOSED state.  SE_CLOSE will not be
+  // signalled as a result of this call.
+  void Close() override;
+
+ private:
+  IceTransportChannel* _ice_channel;
+};
+
 class DtlsTransport : public sigslot::has_slots<>{
 public:
     DtlsTransport(IceTransportChannel* ice_channel);
@@ -40,6 +72,8 @@ private:
 
     bool _setup_dtls();
 
+    bool _maybe_start_dtls();
+
 private:
     IceTransportChannel* _ice_channel;   
     DtlsTransportState _dtls_state = DtlsTransportState::k_new;
@@ -48,6 +82,10 @@ private:
     std::unique_ptr<rtc::SSLStreamAdapter> _dtls;
     rtc::Buffer _cached_client_hello;
     rtc::RTCCertificate* _local_certificate = nullptr;
+    StreamInterfaceChannel* _downward = nullptr;
+    std::string _remote_fingerprint_alg;
+    rtc::Buffer _remote_fingerprint_value;
+
 };
 } // namespace grtc
 
